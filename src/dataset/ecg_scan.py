e4be434e5062eb7b67752d5056a570de14d1eb51
Elias Stenhede <elias.stenhede@gmail.com>
2025-01-16 09:47:35 +0000

    refactor: scans and masks are in same folder

--- src/dataset/ecg_scan.py
+++ src/dataset/ecg_scan.py
@@ -5,18 +5,13 @@ import torch
 
 
 class ECGScanDataset(torch.utils.data.Dataset[Any]):
-    def __init__(
-        self, ecg_scan_path: str, ecg_mask_path: str, transform: Any = None, load_n: Union[None, int] = None
-    ) -> None:
-        self.ecg_scan_path = ecg_scan_path
-        self.ecg_mask_path = ecg_mask_path
+    def __init__(self, data_path: str, transform: Any = None, load_n: Union[None, int] = None) -> None:
+        self.data_path = data_path
         self.transform = transform
 
         # Prepend current software directory if path is not absolute.
-        if not os.path.isabs(self.ecg_scan_path):
-            self.ecg_scan_path = os.path.join(os.path.dirname(__file__), self.ecg_scan_path)
-        if not os.path.isabs(self.ecg_mask_path):
-            self.ecg_mask_path = os.path.join(os.path.dirname(__file__), self.ecg_mask_path)
+        if not os.path.isabs(self.data_path):
+            self.data_path = os.path.join(os.path.dirname(__file__), self.data_path)
 
         self.ecg_scan_files, self.ecg_mask_files = self._find_ecg_files()
         if load_n:
@@ -24,25 +19,24 @@ class ECGScanDataset(torch.utils.data.Dataset[Any]):
             self.ecg_mask_files = self.ecg_mask_files[:load_n]
 
     def _find_ecg_files(self) -> Tuple[List[str], List[str]]:
-        for path in [self.ecg_scan_path, self.ecg_mask_path]:
-            if not os.path.exists(path):
-                raise ValueError(f"Path {path} does not exist.")
+        if not os.path.exists(self.data_path):
+            raise ValueError(f"Path {self.data_path} does not exist.")
 
         def is_scan_file(file: str) -> bool:
             return file.endswith(".png") and not file.endswith("_mask.png")
 
-        ecg_scan_filenames = list(map(lambda x: x.split(".")[0], filter(is_scan_file, os.listdir(self.ecg_mask_path))))
+        ecg_scan_filenames = list(map(lambda x: x.split(".")[0], filter(is_scan_file, os.listdir(self.data_path))))
 
-        ecg_scan_files = list(map(lambda x: os.path.join(self.ecg_scan_path, f"{x}.png"), ecg_scan_filenames))
-        ecg_mask_files = list(map(lambda x: os.path.join(self.ecg_mask_path, f"{x}_mask.png"), ecg_scan_filenames))
+        ecg_scan_files = list(map(lambda x: os.path.join(self.data_path, f"{x}.png"), ecg_scan_filenames))
+        ecg_mask_files = list(map(lambda x: os.path.join(self.data_path, f"{x}_mask.png"), ecg_scan_filenames))
         return ecg_scan_files, ecg_mask_files
 
     def _load_scan(self, file: str) -> torch.Tensor:
-        scan: torch.Tensor = decode_image(os.path.join(self.ecg_scan_path, file), mode="RGB").float() / 255.0
+        scan: torch.Tensor = decode_image(os.path.join(self.data_path, file), mode="RGB").float() / 255.0
         return scan
 
     def _load_mask(self, file: str) -> torch.Tensor:
-        mask: torch.Tensor = decode_image(os.path.join(self.ecg_mask_path, file), mode="RGB").float() / 255.0
+        mask: torch.Tensor = decode_image(os.path.join(self.data_path, file), mode="RGB").float() / 255.0
         mask[0] = 1 - mask[1] - mask[2]  # Fill the red channel so that each pixel sums to 1
         return mask
 
